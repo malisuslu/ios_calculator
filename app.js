59cdfe38736bb5817102213ef17d466d9a22e844
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-05-23 02:25:42 +0300

    Several improvements are applied  on both functionality and display in order to achive the original design of ios calculator

--- app.js
+++ app.js
@@ -57,44 +57,235 @@ onkeydown = function(e) {
     } else if (e.key == 'Escape') {
         clear.click();
     } else if (e.key == 'Backspace') {
-        display.textContent = display.textContent.slice(0, -1);
+        if(result.length > 1 && result[result.length - 1] == operator) {
+            result = result.toString().slice(0, -2);
+            display.textContent = result;
+        } else if (result.length == 1 || result.length == 0) {
+            result = '';
+            display.textContent = "0";
+        } else {
+            result = result.toString().slice(0, -1);
+            display.textContent = result;
+        }
+        display.textContent = display.textContent.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    } else if (e.key == '+/-') {
+        plusminus.click();
     }
 }
 
 
+let result = '';
+let operator = '';
+let isOver = false;
 
-document.querySelectorAll('.main-body > div').forEach(function(elem) {
-
+document.querySelectorAll('.main-body > div:not(.display)').forEach(function(elem) {
     elem.onclick = function(e) {
+
 
         e.target.classList.add('active'); 
         setTimeout(function() {
-            e.target.classList.remove('active');
+        e.target.classList.remove('active');
         }
         , 100);
+        document.querySelector('.clear').textContent = 'C';
 
-        let last_char = display.textContent[display.textContent.length - 1];
+        if (e.target.classList.contains('clear')) {
+            result = '';
+            display.textContent = '0';
+            document.querySelector('.clear').textContent = 'AC';
+            operator = '';
+            isOver = false;
 
-        if (e.target == plus || e.target == minus || e.target == multiply || e.target == divide) {
-            if (last_char == '+' || last_char == '-' || last_char == 'x' || last_char == '÷') {
-                display.textContent = display.textContent.slice(0, -1);
+        } else if (e.target.classList.contains('plusminus')) {
+            if(typeof result != 'number') {
+            result = eval(result.replace(/x/g, "*").replace(/÷/g, "/"));
+            result = result * -1;
+            display.textContent = result;
+
+            } else {
+            result = result * -1;
+            display.textContent = result;
             }
-            display.textContent += e.target.textContent;
+            operator = '';
+            isOver = false;
 
-        } else if (display.textContent.includes('=')) {
-            display.textContent = '';
+        } else if (e.target.classList.contains('percent')) {
+            if(typeof result != 'number') {
+            result = eval(result.replace(/x/g, "*").replace(/÷/g, "/"));
+            result = result / 100;
+            display.textContent = result;
 
-        } else if (e.target != equal && e.target != clear && e.target != display ) {
-        display.textContent += e.target.textContent;
+            } else {
+            result = result / 100;
+            display.textContent = result;
+            }
+            operator = '';
+            isOver = true;
 
-        } else if (e.target == equal) {
-            res_string = display.innerHTML.replace(/x/g, "*").replace(/÷/g, "/").replace(/%/g, "/100").replace(/±/g, "-");
-            console.log(res_string);
-            display.textContent = e.target.textContent + parseFloat(eval(res_string).toFixed(6));
+        } else if (e.target.classList.contains('main')) {
+            if(result[result.length - 1] == '+' || result[result.length - 1] == '-' || result[result.length - 1] == 'x' || result[result.length - 1] == '÷') {
+                result = result.slice(0, -1);
+                result = result + e.target.textContent;
 
-        } else if (e.target == clear) {
-            display.textContent = "";
+            } else if (result.toString().includes('+') || result.toString().includes('-') || result.toString().includes('x') || result.toString().includes('÷')) {
+                result = eval(result.toString().replace(/x/g, "*").replace(/÷/g, "/"));
+                display.textContent = parseFloat(result.toFixed(5));
+                result = result + e.target.textContent;   
 
+            } else {
+            result = result + e.target.textContent;
+            }
+            operator = e.target.textContent;
+
+        } else if (e.target.classList.contains('number')) {
+            
+            if(result.includes('+') || result.includes('-') || result.includes('x') || result.includes('÷')) {
+                result = result + e.target.textContent;
+                display.textContent = result.slice(result.indexOf(operator)+1, result.length);
+
+            } else if(isOver) { 
+                result = '';
+                isOver = false;
+
+            } else if (result.toString() == '0') {
+                result = e.target.textContent;
+                display.textContent = result;
+
+            } else {
+            result = result + e.target.textContent;
+            display.textContent = result;
+            }
+
+        } else if (e.target.classList.contains('dot')) {
+            if (result.includes('.')) {
+                return;
+            } else {
+            result = result + e.target.textContent;
+            display.textContent = result;
+            }
+
+        } else {
+            result = eval(result.toString().replace(/x/g, "*").replace(/÷/g, "/"));
+            display.textContent = parseFloat(result.toFixed(5));
+            isOver = true;
+            operator = '';
         }
+        
+        display.textContent = display.textContent.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        
+    displaySize();
+    console.log(result);
     }
 });
+
+function displaySize() {
+    let display = document.querySelector('.display');
+    let displayWidth = display.offsetWidth;
+    // console.log(displayWidth);
+    let displayHeight = display.offsetHeight;
+    // console.log(displayHeight);
+    let fontSize = displayWidth / (display.textContent.length/1.8);
+    if (fontSize > displayHeight) {
+        display.style.fontSize = displayHeight + 'px';
+    } else {
+        display.style.fontSize = fontSize + 'px';
+    }
+}
+
+
+// let number = '';
+// let next_number = '';
+// let operator = '';
+// let result = '';
+
+// document.querySelectorAll('.main-body > div').forEach(function(elem) {
+//     elem.onclick = function(e) {
+
+//         e.target.classList.add('active'); 
+//         setTimeout(function() {
+//         e.target.classList.remove('active');
+//         }
+//         , 100);
+//         document.querySelector('.clear').textContent = 'C';
+
+
+//         if (e.target.classList.contains('clear')) {
+//             result = '';
+//             display.textContent = '0';
+//             document.querySelector('.clear').textContent = 'AC';
+
+//         } else if (e.target.classList.contains('plusminus')) {
+//             result = result * -1;
+//             display.textContent = result;
+
+//         } else if (e.target.classList.contains('percent')) {
+//             result = result / 100;
+//             display.textContent = result;
+
+//         } else if (e.target.classList.contains('main')) {
+//             operator = e.target.textContent;
+
+//         } else if (e.target.classList.contains('number')) {
+//             if(operator == '') {
+//                 number = number + e.target.textContent;
+//                 display.textContent = number;
+
+//             } else {
+//             next_number = next_number + e.target.textContent;
+//             display.textContent = next_number;
+//             }
+
+//         } else if (e.target.classList.contains('dot')) {
+//             if (operator == '' && number.includes('.') ) {
+//                 return;
+
+//             } else if (operator != '' && next_number.includes('.') ) {
+//                 return;
+
+//             } else if (operator == '' && !number.includes('.') ) {
+//                 number = number + e.target.textContent;
+//                 display.textContent = number;
+
+//             } else if (operator != '' && !next_number.includes('.') ) {
+//                 next_number = next_number + e.target.textContent;
+//                 display.textContent = next_number;
+//             }
+
+//         } else {
+//             if (operator == '') {
+//                 result = number;
+//                 number = '';
+//                 next_number = '';
+//                 operator = e.target.textContent;
+//                 display.textContent = operator;
+
+//             } else {
+//                 result = result + next_number;
+//                 next_number = '';
+//                 operator = e.target.textContent;
+//                 display.textContent = operator;
+//             }
+
+//             result = eval(result.replace(/x/g, "*").replace(/÷/g, "/"));
+//             display.textContent = parseFloat(result.toFixed(5));
+//         }
+
+//     }
+// });
+
+// //number with c
+// function nwc(x) {
+//     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
