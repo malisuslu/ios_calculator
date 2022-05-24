fabeac14cb717183a60d2554f8845751b2b1b043
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-05-24 13:13:41 +0300

    Further improvements are applied based on additional feedbacks

--- app.js
+++ app.js
@@ -15,9 +15,58 @@ let divide = document.querySelector('.divide');
 let equal = document.querySelector('.equal');
 let clear = document.querySelector('.clear');
 let display = document.querySelector('.display');
-let dot = document.querySelector('.dot');
+let comma = document.querySelector('.comma');
 let percent = document.querySelector('.percent');
 let plusminus = document.querySelector('.plusminus');
+
+/*
+function rpl(str) {
+    return str.replace('*', 'x').replace('/', '÷').replace('Escape', 'C').replace('Enter', '=').replace('.', ',');
+}
+
+onkeydown = function(e) {
+
+    let key = "(//div/div[text()='" + rpl(e.key) + "'])[1]";
+    console.log(key);   
+   
+
+    let elm = document.evaluate(key, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    if (elm) {
+        e.target.classList.contains('main') ? display.textContent = 'err' : null;
+        elm.click();
+        console.log(elm);
+    }
+
+    if (e.key == 'Backspace') {
+        if(result.length > 2 && result[result.length - 1] == operator) {
+            result = result.toString().slice(0, -2);
+            display.textContent = result;
+            operator = '';
+
+        } else if (result.length == 1 || result.length == 0 || result[0] == '-') {
+            result = '0';
+            display.textContent = result;
+            operator = '';
+
+        } else if (result.toString().includes('+') || result.toString().includes('-') || result.toString().includes('*') || result.toString().includes('/')) {
+            result = result.toString().slice(0, -1);
+            display.textContent = result.slice(result.lastIndexOf(operator)+1, result.length);
+        } else {
+            result = result.toString().slice(0, -1);
+            display.textContent = result;
+        }
+
+        display.textContent = result.toString().replace('.', ',').replace(/\B(?<!\,\d*)(?=(\d{3})+(?!\d))/g, ".");
+
+        holdButton();
+        displaySize();
+
+        console.log(result);
+        console.log(operator);
+        }
+};
+*/
 
 onkeydown = function(e) {
     if (e.key == '0') {
@@ -50,28 +99,43 @@ onkeydown = function(e) {
         divide.click();
     } else if (e.key == 'Enter') {
         equal.click();
-    } else if (e.key == '.') {
-        dot.click();
+    } else if (e.key == ',') {
+        comma.click();
     } else if (e.key == '%') {
         percent.click();
     } else if (e.key == 'Escape') {
         clear.click();
     } else if (e.key == 'Backspace') {
-        if(result.length > 1 && result[result.length - 1] == operator) {
+        if(result.length > 2 && result[result.length - 1] == operator) {
             result = result.toString().slice(0, -2);
             display.textContent = result;
-        } else if (result.length == 1 || result.length == 0) {
-            result = '';
-            display.textContent = "0";
+            operator = '';
+
+        } else if (result.length == 1 || result.length == 0 || result[0] == '-') {
+            result = '0';
+            display.textContent = result;
+            operator = '';
+
+        } else if (result.toString().includes('+') || result.toString().includes('-') || result.toString().includes('*') || result.toString().includes('/')) {
+            result = result.toString().slice(0, -1);
+            display.textContent = result.slice(result.lastIndexOf(operator)+1, result.length);
         } else {
             result = result.toString().slice(0, -1);
             display.textContent = result;
         }
-        display.textContent = display.textContent.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        display.textContent = result.toString().replace('.', ',').replace(/\B(?<!\,\d*)(?=(\d{3})+(?!\d))/g, ".");
+
+        holdButton();
+        displaySize();
+
+        console.log(result);
+        console.log(operator);
+
     } else if (e.key == '+/-') {
         plusminus.click();
     }
-}
+};
 
 
 let result = '';
@@ -90,7 +154,7 @@ document.querySelectorAll('.main-body > div:not(.display)').forEach(function(ele
         document.querySelector('.clear').textContent = 'C';
 
         if (e.target.classList.contains('clear')) {
-            result = '';
+            result = '0';
             display.textContent = '0';
             document.querySelector('.clear').textContent = 'AC';
             operator = '';
@@ -98,7 +162,7 @@ document.querySelectorAll('.main-body > div:not(.display)').forEach(function(ele
 
         } else if (e.target.classList.contains('plusminus')) {
             if(typeof result != 'number') {
-            result = eval(result.replace(/x/g, "*").replace(/÷/g, "/"));
+            result = eval(result.replaceAll(/x/g, "*").replaceAll(/÷/g, "/"));
             result = result * -1;
             display.textContent = result;
 
@@ -106,19 +170,22 @@ document.querySelectorAll('.main-body > div:not(.display)').forEach(function(ele
             result = result * -1;
             display.textContent = result;
             }
+
             operator = '';
             isOver = false;
 
         } else if (e.target.classList.contains('percent')) {
+
             if(typeof result != 'number') {
-            result = eval(result.replace(/x/g, "*").replace(/÷/g, "/"));
-            result = result / 100;
+            result = eval(result.replaceAll(/x/g, "*").replaceAll(/÷/g, "/"));
+            result = parseFloat(result.toFixed(10)) / 100;
             display.textContent = result;
 
             } else {
-            result = result / 100;
+            result = parseFloat(result.toFixed(10)) / 100;
             display.textContent = result;
             }
+
             operator = '';
             isOver = true;
 
@@ -130,14 +197,20 @@ document.querySelectorAll('.main-body > div:not(.display)').forEach(function(ele
             } else if (result.toString().includes('+') || result.toString().includes('-') || result.toString().includes('x') || result.toString().includes('÷')) {
                 if (result.toString()[0] == '-') {
                     result = result + e.target.textContent;
+
+                } else if (result.length >= 2 && result.toString()[result.length-1] == '0' && result.toString()[result.length-2] == '÷') {
+                    result = '0';
+                    display.textContent = 'undefined';
+                
                 } else {
-                    result = eval(result.toString().replace(/x/g, "*").replace(/÷/g, "/"));
-                    display.textContent = parseFloat(result.toFixed(5));
+                    result = eval(result.toString().replaceAll(/x/g, "*").replaceAll(/÷/g, "/"));
+                    display.textContent = parseFloat(result.toFixed(10)).toString().replace('.', ',');
                     result = result + e.target.textContent;   
                 }
 
             } else {
                 result = result + e.target.textContent;
+
             }
 
             operator = e.target.textContent;
@@ -145,15 +218,26 @@ document.querySelectorAll('.main-body > div:not(.display)').forEach(function(ele
         } else if (e.target.classList.contains('number')) {
             
             if(result.toString().includes('+') || result.toString().includes('-') || result.toString().includes('x') || result.toString().includes('÷')) {
-                if (result.toString()[0] == '-') {
+                if (result.toString()[0] == '-' && !operator) {
+                    result = result + e.target.textContent;
+                    display.textContent = result.slice(0, result.length);
+
+                } else if (result.toString()[0] == '-' && operator) {
+                    result = result + e.target.textContent;
+                    display.textContent = result.slice(result.toString().lastIndexOf(operator)+1, result.length);
+                
+                } else if (result.toString()[result.length-1] == '0' && result.toString()[result.length-2] == operator) {
+                    result = result.toString().slice(0, -1);
                     result = result + e.target.textContent;
                     display.textContent = result.slice(result.lastIndexOf(operator)+1, result.length);
                 } else {
                 result = result + e.target.textContent;
                 display.textContent = result.slice(result.indexOf(operator)+1, result.length);
                 }
+
             } else if(isOver) { 
-                result = '';
+                result = e.target.textContent;
+                display.textContent = result;
                 isOver = false;
 
             } else if (result.toString() == '0') {
@@ -165,23 +249,54 @@ document.querySelectorAll('.main-body > div:not(.display)').forEach(function(ele
             display.textContent = result;
             }
 
-        } else if (e.target.classList.contains('dot')) {
-            if (result.includes('.')) {
+        } else if (e.target.classList.contains('comma')) {
+            if (result.toString().includes('.')) {
+                if (result.toString().includes('+') || result.toString().includes('-') || result.toString().includes('x') || result.toString().includes('÷')) {
+                    if (result.toString().lastIndexOf(operator) < result.toString().lastIndexOf('.')) {
+                        return;
+
+                    } else if(result.toString()[0] == '-' && !operator) {
+                        result = result;
+                        display.textContent = result.toString().slice(0, result.length);
+                        
+                    } else {
+                        result = result + ".";
+                        display.textContent = result.toString().slice(result.toString().lastIndexOf(operator)+1, result.length);
+                    }
+                    
+                } else {
                 return;
-            } else {
-            result = result + e.target.textContent;
-            display.textContent = result;
+                }
+
+            } else if (operator){
+                result = result + ".";
+                display.textContent = result.slice(result.indexOf(operator)+1, result.length);
+
+            } else{
+                result = result + ".";
+                display.textContent = result;
             }
 
         } else {
-            result = eval(result.toString().replace(/x/g, "*").replace(/÷/g, "/"));
-            display.textContent = parseFloat(result.toFixed(5));
+            if (result.length >= 2 && result.toString()[result.length-1] == '0' && result.toString()[result.length-2] == '÷') {
+                result = '0';
+                display.textContent = 'undefined';
+
+            } else {result = eval(result.toString().replaceAll(/x/g, "*").replaceAll(/÷/g, "/"));
+            display.textContent = parseFloat(result.toFixed(10));
             isOver = true;
             operator = '';
+            }
         }
-        
-        display.textContent = display.textContent.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    
+
+        if (e.target.classList.contains('main')) {
+            display.textContent = display.textContent.replace(/\B(?<!\,\d*)(?=(\d{3})+(?!\d))/g, ".");
+        } else {
+            display.textContent = display.textContent.replace('.',',').replace(/\B(?<!\,\d*)(?=(\d{3})+(?!\d))/g, ".");
+        }
+
+        display.textContent[0] == ',' ? display.textContent = '0' + display.textContent : display.textContent;
+
     holdButton();
     displaySize();
     console.log(result);
@@ -202,124 +317,14 @@ function displaySize() {
     }
 }
 
-function holdButton(e) {
-    if (operator != '+' && operator != '-' && operator != 'x' && operator != '÷') {
-        document.querySelectorAll('.main').forEach(function(elem) { 
-            elem.style.opacity = '1';
-        });
-    } else if(operator == '+') {
-    document.querySelectorAll('.main').forEach(function(elem) { 
-        elem.style.opacity = '1';});
-    document.querySelector('.plus').style.opacity = '0.5';
-    } else if(operator == '-') {
-    document.querySelectorAll('.main').forEach(function(elem) { 
-        elem.style.opacity = '1';});
-    document.querySelector('.minus').style.opacity = '0.5';
-    } else if(operator == 'x') {
-    document.querySelectorAll('.main').forEach(function(elem) { 
-        elem.style.opacity = '1';});
-    document.querySelector('.multiply').style.opacity = '0.5';
-    } else if(operator == '÷') {
-    document.querySelectorAll('.main').forEach(function(elem) { 
-        elem.style.opacity = '1';});
-    document.querySelector('.divide').style.opacity = '0.5';
-    }
-}
 
-
-// let number = '';
-// let next_number = '';
-// let operator = '';
-// let result = '';
-
-// document.querySelectorAll('.main-body > div').forEach(function(elem) {
-//     elem.onclick = function(e) {
-
-//         e.target.classList.add('active'); 
-//         setTimeout(function() {
-//         e.target.classList.remove('active');
-//         }
-//         , 100);
-//         document.querySelector('.clear').textContent = 'C';
-
-
-//         if (e.target.classList.contains('clear')) {
-//             result = '';
-//             display.textContent = '0';
-//             document.querySelector('.clear').textContent = 'AC';
-
-//         } else if (e.target.classList.contains('plusminus')) {
-//             result = result * -1;
-//             display.textContent = result;
-
-//         } else if (e.target.classList.contains('percent')) {
-//             result = result / 100;
-//             display.textContent = result;
-
-//         } else if (e.target.classList.contains('main')) {
-//             operator = e.target.textContent;
-
-//         } else if (e.target.classList.contains('number')) {
-//             if(operator == '') {
-//                 number = number + e.target.textContent;
-//                 display.textContent = number;
-
-//             } else {
-//             next_number = next_number + e.target.textContent;
-//             display.textContent = next_number;
-//             }
-
-//         } else if (e.target.classList.contains('dot')) {
-//             if (operator == '' && number.includes('.') ) {
-//                 return;
-
-//             } else if (operator != '' && next_number.includes('.') ) {
-//                 return;
-
-//             } else if (operator == '' && !number.includes('.') ) {
-//                 number = number + e.target.textContent;
-//                 display.textContent = number;
-
-//             } else if (operator != '' && !next_number.includes('.') ) {
-//                 next_number = next_number + e.target.textContent;
-//                 display.textContent = next_number;
-//             }
-
-//         } else {
-//             if (operator == '') {
-//                 result = number;
-//                 number = '';
-//                 next_number = '';
-//                 operator = e.target.textContent;
-//                 display.textContent = operator;
-
-//             } else {
-//                 result = result + next_number;
-//                 next_number = '';
-//                 operator = e.target.textContent;
-//                 display.textContent = operator;
-//             }
-
-//             result = eval(result.replace(/x/g, "*").replace(/÷/g, "/"));
-//             display.textContent = parseFloat(result.toFixed(5));
-//         }
-
-//     }
-// });
-
-// //number with c
-// function nwc(x) {
-//     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-// }
-
-
-
-
-
-
-
-
-
-
-
-
+function holdButton() {
+    document.querySelectorAll('.main').forEach(function(elem) {
+        if (elem.textContent == operator) {
+            elem.style.backgroundColor = 'white';
+            elem.style.color = '#F1A33C';
+        } else {
+            elem.style.backgroundColor = '#F09114';
+            elem.style.color = 'white';
+        }
+    })};
